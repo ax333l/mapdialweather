e4be01b31f1a641f96d997825f5cce94e8117c80
ax333l <51031206+ax333l@users.noreply.github.com>
2021-03-06 13:34:03 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -6,15 +6,12 @@ const bodyParser = require("body-parser")
 const cheerio = require('cheerio')
 const fs = require('fs')
 let cities = require('./weather.json')
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: false })
 const uuid = require('uuid-random')
-
 
 app.use(bodyParser.json());
 
-let api_key = '8723Z30-135M24K-K255VTS-91F90K1'
-const apiKey = '1WMMX7z7ESKEZYwBP7XrxuPCVznXVoN4Ol9_0YT67-4'
+let api_key = process.env.API_KEY
+const apiKey = process.env.HERE_KEY
 
 app.post('/api_key', async function(req, res){
     if(req.body.api_key){
@@ -60,15 +57,7 @@ app.delete('/', async function(req, res){
     }
 })
 
-/*app.post('/cities', async function(req, res){
-    if(req.body.cities){
-        cities = req.body.cities
-        res.status(200).send({})
-    }
-})*/
-
 const server = 'https://fasfsa-scriptjs.e4ff.pro-eu-west-1.openshiftapps.com/api/public/map'
-//const server = 'http://localhost:3000/api/public/map'
 
 function has(object, key) {
     return object ? hasOwnProperty.call(object, key) : false;
@@ -116,21 +105,6 @@ async function marker(text, method){
 
 async function getYandexUrl(name){
     return new Promise(async (resolve, reject) => {
-       /* nightmare
-            .goto('https://yandex.by/pogoda/minsk')
-            .evaluate(function() {
-                document.querySelector('#header2input').value = ''
-            })            
-            .type('#header2input', name)
-            .wait('#search-results > li:nth-child(1) > a')
-            .evaluate(() => document.querySelector('#search-results > li:nth-child(1) > a').href)
-            .then(link => {
-                console.log(link)
-                resolve(link)
-            })
-            .catch(error => {
-                console.error(error)
-            })*/
         request({url: 'https://yandex.by/pogoda/search?request='+encodeURI(name)}, function(err, res, html) {
             let $ = cheerio.load(html)
             let url = $('body > div > div.content.content ancient-design yes > div.grid.clearfix > div > div.grid__cell.grid__cell_pos_1.grid__cell_size_4 > div > li > a').attr('href') || $('body > div > div.content.content_ancient-design_yes > div.grid.clearfix > div > div.grid__cell.grid__cell_pos_1.grid__cell_size_4 > div > li:nth-child(1) > a').attr('href')
@@ -149,7 +123,7 @@ function getLocationbyAdress(address, callback){
         }
     })
 }
-//body > section > div.content_wrap > div > div.main > div > div.__frame_sm > div.forecast_frame.forecast_now > div.tabs._left > div > div > div.tab-content > div.tab-weather > div.js_meas_container.temperature.tab-weather__value > span.unit.unit_temperature_c > span
+
 function weatherConditionsNow(link, callback){
     request({url: link}, function(err,res,html){
         var $ = cheerio.load(html)
@@ -161,13 +135,10 @@ function weatherConditionsNow(link, callback){
         callback(conditions)
     })
 }
-//'https://yandex.by/pogoda/minsk','https://yandex.by/pogoda/vitebsk','https://yandex.by/pogoda/grodno','https://yandex.by/pogoda/brest','https://yandex.by/pogoda/mogilev','https://yandex.by/pogoda/gomel','https://yandex.by/pogoda/26003','https://yandex.by/pogoda/26001'
+
 setInterval(() => {
     cities.forEach(async (city,i) => {
-        console.log(city)
-        console.log(city.link)
         if(!city.link){
-            console.log(1)
             let uri = await getYandexUrl(city.city)
             cities[i].link = uri
             city.link = uri
@@ -203,7 +174,7 @@ setInterval(() => {
                     fs.writeFileSync('./weather.json', JSON.stringify(cities))
                 }
                 else{
-                    console.log('place not found')
+                    console.log(`place ${city} not found`)
                 }
             })
         }
